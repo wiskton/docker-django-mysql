8c95039498fadd26387399004cae083935499279
Willem Allan <willemarf@gmail.com>
2022-02-04 23:44:01 -0300

    heroku

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0)) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
