5142520f867db5a05d1dee16a320fd4afa6a5354
Willem Allan <willemarf@gmail.com>
2022-02-04 23:47:36 -0300

    heroku

--- project/project/settings.py
+++ project/project/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY") 
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ") 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0)) 
 
